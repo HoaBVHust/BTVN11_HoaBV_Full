4bce7ff472219536b91d0d00036e2af2e0dbf38d
HoaBVHust <hoabv.hust@gmail.com>
2019-09-22 20:58:36 +0700

    init

---
+++ src/main/java/com/topica/edu/itlab/jdbc/load/Load.java
@@ -0,0 +1,72 @@
+package com.topica.edu.itlab.jdbc.load;
+
+import java.lang.reflect.Field;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import com.topica.edu.itlab.jdbc.tutorial.annotation.Column;
+import com.topica.edu.itlab.jdbc.tutorial.entity.ClassEntity;
+import com.topica.edu.itlab.jdbc.tutorial.entity.StudentEntity;
+
+public class Load {
+	
+	public static <T> void loadList(ResultSet rs,List<T> listT,Class<T> type){
+		try {
+			while(rs.next()) {
+				T t = type.newInstance();
+				for (Field field : type.getDeclaredFields()) {
+					field.setAccessible(true);
+					Column column = field.getAnnotation(Column.class);
+					Object value = null;
+					if(column!=null) {
+						value =rs.getObject(column.name(), field.getType());			
+					}
+					field.set(t, value);
+				}
+				listT.add(t);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	/*
+	 * Eager load
+	 */
+	public static void loadListEager(ResultSet rs,List<ClassEntity> listClassEntity) {
+		Map<Long, ClassEntity> hashMap = new HashMap<>();
+		
+		try {
+			while(rs.next()) {
+				Long class_id =rs.getLong("class_id");
+				if(!hashMap.keySet().contains(class_id)) {
+					ClassEntity classEntity = new ClassEntity();
+					classEntity.setId(class_id);
+					classEntity.setName(rs.getString("class_name"));
+					classEntity.setListStudent(new ArrayList<StudentEntity>());
+					StudentEntity studentEntity = new StudentEntity();
+					studentEntity.setId(rs.getLong("student_id"));
+					studentEntity.setName(rs.getString("student_name"));
+					classEntity.getListStudent().add(studentEntity);
+					hashMap.put(class_id, classEntity);
+				}
+				else {
+					StudentEntity studentEntity = new StudentEntity();
+					studentEntity.setId(rs.getLong("student_id"));
+					studentEntity.setName(rs.getString("student_name"));
+					hashMap.get(class_id).getListStudent().add(studentEntity);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			for (Entry<Long, ClassEntity> entry: hashMap.entrySet()) {
+				listClassEntity.add(entry.getValue());
+			}
+		}
+	}
+}
---
+++ src/main/java/com/topica/edu/itlab/jdbc/tutorial/App.java
@@ -0,0 +1,76 @@
+package com.topica.edu.itlab.jdbc.tutorial;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import com.topica.edu.itlab.jdbc.load.Load;
+import com.topica.edu.itlab.jdbc.tutorial.entity.ClassEntity;
+import com.topica.edu.itlab.jdbc.tutorial.entity.StudentEntity;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String url = "jdbc:mysql://localhost:3306/";
+        String dbName = "school";
+        String driver = "com.mysql.jdbc.Driver";
+        String userName = "root";
+        String dbpassword = "";
+        String query1 = "SELECT * from Class";
+        String query2 = "Select c.id as class_id,c.name as class_name,s.id as student_id, s.name as student_name from Class c, Student s where c.id = s.class_id";
+        try {
+        	//loading driver
+			Class.forName(driver);
+			// Connection set up with database named as user
+			Connection conn = DriverManager.getConnection(url+dbName, userName, dbpassword);
+			// Creating statement for the connection to use sql queries
+			Statement st = conn.createStatement();
+			
+			/*
+			 * 1. Lazy load
+			 */
+			System.out.println("Lazy load:");
+			ResultSet rs = st.executeQuery(query1);			
+			List<ClassEntity> listClassEntity = new ArrayList<ClassEntity>();
+			//Lazy load data from database to listClassEntity
+			Load.loadList(rs, listClassEntity,ClassEntity.class);
+			System.out.println("when lazy load: "+listClassEntity);
+			if(listClassEntity != null) {
+				for (ClassEntity classEntity : listClassEntity) {
+					//when read data of every ClassEntity, it listStudent = null, load data from student table in database
+					if(classEntity.getListStudent() == null) {
+						classEntity.setListStudent(new ArrayList<StudentEntity>());
+						ResultSet rsStudent = st.executeQuery("SELECT * from Student WHERE class_id="+classEntity.getId());
+						Load.loadList(rsStudent,classEntity.getListStudent(), StudentEntity.class);
+					}
+				}
+				System.out.println("When read every data: "+listClassEntity);
+			}
+			else {
+				System.out.println("List ClassEntity is null!");
+			}
+			
+			/*
+			 * 2. Eager load
+			 */
+			System.out.println("Eager load:");
+			ResultSet rs2 =st.executeQuery(query2);
+			List<ClassEntity> listClassEntity2 = new ArrayList<ClassEntity>();
+			Load.loadListEager(rs2,listClassEntity2);
+			System.out.println(listClassEntity2);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			// TODO: handle exception
+		}
+    }
+}
